298b5594dc2a576ec3c566134bafd58b96f10919
Vikas Patel <vikaspatel7783@gmail.com>
2023-09-07 16:23:55 +0530

    Initial commit

---
+++ order-service/src/test/java/com/vikas/rabbitmq/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vikas.rabbitmq.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ stock-service/src/test/java/com/vikas/rabbitmq/stockservice/StockServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vikas.rabbitmq.stockservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
